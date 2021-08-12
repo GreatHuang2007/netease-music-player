2c08a9619bc768a05b3026f3f36850f3c6037a65
GreatHuang2007 <GreatHuang2007@HUANG-NOTE-02>
2021-08-12 18:17:42 +0800

    添加项目文件。

---
+++ NeteaseMuisc/Music.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace NeteaseMuisc
+{
+    partial class Music
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Music));
+            this.list_message = new System.Windows.Forms.ListBox();
+            this.music_play = new AxWMPLib.AxWindowsMediaPlayer();
+            this.music_name_s = new System.Windows.Forms.TextBox();
+            this.search = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.music_play)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // list_message
+            // 
+            this.list_message.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.list_message.FormattingEnabled = true;
+            this.list_message.ItemHeight = 15;
+            this.list_message.Location = new System.Drawing.Point(16, 60);
+            this.list_message.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.list_message.Name = "list_message";
+            this.list_message.ScrollAlwaysVisible = true;
+            this.list_message.Size = new System.Drawing.Size(569, 319);
+            this.list_message.TabIndex = 1;
+            this.list_message.SelectedIndexChanged += new System.EventHandler(this.list_name_SelectedIndexChanged);
+            this.list_message.DoubleClick += new System.EventHandler(this.list_name_DoubleClick);
+            // 
+            // music_play
+            // 
+            this.music_play.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.music_play.Enabled = true;
+            this.music_play.Location = new System.Drawing.Point(0, 401);
+            this.music_play.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.music_play.Name = "music_play";
+            this.music_play.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("music_play.OcxState")));
+            this.music_play.Size = new System.Drawing.Size(603, 45);
+            this.music_play.TabIndex = 2;
+            // 
+            // music_name_s
+            // 
+            this.music_name_s.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.music_name_s.Location = new System.Drawing.Point(16, 15);
+            this.music_name_s.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.music_name_s.Name = "music_name_s";
+            this.music_name_s.Size = new System.Drawing.Size(485, 25);
+            this.music_name_s.TabIndex = 3;
+            this.music_name_s.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // search
+            // 
+            this.search.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.search.Location = new System.Drawing.Point(511, 15);
+            this.search.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(76, 25);
+            this.search.TabIndex = 4;
+            this.search.Text = "搜索";
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // Music
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 446);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.music_name_s);
+            this.Controls.Add(this.music_play);
+            this.Controls.Add(this.list_message);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Music";
+            this.Text = "Netease Music Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.music_play)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox list_message;
+        private AxWMPLib.AxWindowsMediaPlayer music_play;
+        private System.Windows.Forms.TextBox music_name_s;
+        private System.Windows.Forms.Button search;
+    }
+}
---
+++ NeteaseMuisc/Music.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+
+namespace NeteaseMuisc
+{
+    public partial class Music : Form
+    {
+        public Music()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void list_name_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void list_name_DoubleClick(object sender, EventArgs e)//双击列表的事件
+        {
+            var address = new Regex("!(.*?)!");//这里的理解和下面一样
+            MatchCollection matches_name = address.Matches(this.list_message.SelectedItem.ToString());
+            foreach (Match m in matches_name)
+            {
+                music_play.URL = string.Format("{0}", m.Groups[1].Value);//调用MediaPlayer播放获取到的链接                music_play.Ctlcontrols.play();
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)//这个用不到
+        {
+
+        }
+
+        private void search_Click(object sender, EventArgs e)//这里是搜索事件（核心）
+        {
+            list_message.Items.Clear();
+            list_message.Items.Add("正在搜索……");
+            var api = new NeteaseMusicAPI();//这里用到下面的两个Class
+            var apires = api.Search(music_name_s.Text);//传入内容
+            var songmessage = "";//搜到的歌的信息先弄一个var
+            foreach (var song in apires.Result.Songs)//循环读取歌曲信息
+            {
+                songmessage += string.Format("@{0} - {1} !{2}! #", song.Name, song.Ar[0].Name, api.GetSongsUrl(new long[] { song.Id }).Data[0].Url);
+            }//第一个数据是规则这里我引入两个符号方便读取之间的内容
+
+            var web = new Regex("@(.*?)#");//读取规则@和#之间的内容
+            MatchCollection matches_web = web.Matches(songmessage);
+            list_message.Items.Clear();
+            foreach (Match m in matches_web)//循环读取内容
+            {
+                list_message.Items.Add(string.Format("{0}", m.Groups[1].Value));//添加到list中
+            }
+        }
+    }
+
+
+}
